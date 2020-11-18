2bab92fdba50f464db012441e36b56cb75cb4b68
Nikhil's Empire <nikhil.cse16@gmail.com>
2020-11-18 23:10:02 +0530

    Initial commit in progress

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,send_file,request
+from chatbot import chatbot
+app=Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template("index.html")
+#
+# @app.route('/Resume.pdf')
+# def Resume():
+#     p="Resume.pdf"
+#     return send_file(p,as_attachment=True)
+
+@app.route("/")
+def get_bot_response():
+    return render_template("MoreAboutMe.html")
+
+@app.route("/get")
+def getbotresponse():
+    userText = request.form.get('msg')
+    print(userText)
+    return str(chatbot.get_response(userText))
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
---
+++ static/scripts.js
@@ -0,0 +1,36 @@
+
+console.log("I'm a !Stupid not here")
+
+let theme =localStorage.getItem('theme')
+
+if (theme ==null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		//setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href ="{{url_for('static',filename='CSS/default.css')}}"
+	}
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href ="{{url_for('static',filename='CSS/blue.css')}}"
+	}
+	if(mode == 'green'){
+		document.getElementById('theme-style').href="{{ url_for('static',filename='CSS/green.css')}}"
+	}
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href="{{url_for('static',filename='CSS/purple.css')}}"
+	}
+	localStorage.setItem('theme', mode)
+}
