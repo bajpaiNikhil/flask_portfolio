63bebbc336f75418db4a0d31469b0a0bc1293310
Nikhil's Empire <nikhil.cse16@gmail.com>
2020-11-21 23:53:35 +0530

    inprogress 1.1

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 from flask import Flask,render_template,send_file,request
-from chatbot import chatbot
+#from chatbot import chatbot
 app=Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return render_template("index.html")
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/Resume.pdf')
+def Resume():
+    p="Resume.pdf"
+    return send_file(p,as_attachment=True)
+
+@app.route('/form',methods=["POST"])
+def form():
+    Name=request.form.get("Name")
+
+    Email = request.form.get("Email")
+    Message = request.form.get("Message")
+    return  render_template("form.html", Name=Name,Email=Email,Message=Message)
+
+# @app.route("/")
+# def get_bot_response():
+#     return render_template("MoreAboutMe.html")
 #
-# @app.route('/Resume.pdf')
-# def Resume():
-#     p="Resume.pdf"
-#     return send_file(p,as_attachment=True)
-
-@app.route("/")
-def get_bot_response():
-    return render_template("MoreAboutMe.html")
-
-@app.route("/get")
-def getbotresponse():
-    userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
+# @app.route("/get")
+# def getbotresponse():
+#     userText = request.args.get('msg')
+#     return str(chatbot.get_response(userText))
 
 
 
