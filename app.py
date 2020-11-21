58624d755b51525a61f1fc682ca7b7b285b54f24
Nikhil's Empire <nikhil.cse16@gmail.com>
2020-11-21 21:27:09 +0530

    inprogress 1.1

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from flask import Flask,render_template,send_file,request
 from chatbot import chatbot
 app=Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-@app.route('/Resume.pdf')
-def Resume():
-    p="Resume.pdf"
-    return send_file(p,as_attachment=True)
-
-# @app.route("/")
-# def get_bot_response():
-#     return render_template("MoreAboutMe.html")
+# @app.route('/')
+# def home():
+#     return render_template("index.html")
 #
-# @app.route("/get")
-# def getbotresponse():
-#     userText = request.args.get('msg')
-#     return str(chatbot.get_response(userText))
+# @app.route('/Resume.pdf')
+# def Resume():
+#     p="Resume.pdf"
+#     return send_file(p,as_attachment=True)
+
+@app.route("/")
+def get_bot_response():
+    return render_template("MoreAboutMe.html")
+
+@app.route("/get")
+def getbotresponse():
+    userText = request.args.get('msg')
+    return str(chatbot.get_response(userText))
 
 
 
