18105d4e84aee5620c1a64ed8eb223618251fd48
Nikhil's Empire <nikhil.cse16@gmail.com>
2020-11-22 13:05:24 +0530

    inprogress 1.1.1

--- static/scripts.js
+++ static/scripts.js
@@ -15,7 +15,7 @@ for (var i=0; themeDots.length > i; i++){
 	themeDots[i].addEventListener('click', function(){
 		let mode = this.dataset.mode
 		console.log('Option clicked:', mode)
-		//setTheme(mode)
+		setTheme(mode)
 	})
 }
 
